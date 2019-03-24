b114890599dd5b99b48462ffaea2a6e9aefb0c11
mkrajnak <mkrajnak>
2019-03-25 00:15:55 +0100

    Decrypting files

--- solution.py
+++ solution.py
@@ -5,22 +5,56 @@ SUB = [0, 1, 1, 0, 1, 0, 1, 0]
 N_B = 32
 N = 8 * N_B
 
-def get_contents(f_name):
-    return
+
+# Next keystream, taken from decrypted file
+def step(x):
+  x = (x & 1) << N+1 | x << 1 | x >> N-1
+  y = 0
+  for i in range(N):
+    y |= SUB[(x >> i) & 7] << i
+  return y
 
 
-def main():
-    plain = open('bis.txt', 'rb').read()
-    secret = open('bis.txt.enc', 'rb').read()
-    super_cipher = open('super_cipher.py.enc', 'rb').read()
-
-    length = len(plain) if len(plain) < len(secret) else len(secret)
-    keystream = []
+# xor or two byte arrays
+def xor(ar1, ar2):
+    length = len(ar1) if len(ar1) < len(ar2) else len(ar2)
+    res = []
     for i in range(length):
-            keystream.append(plain[i] ^ secret[i] ^ super_cipher[i])
+            res.append(ar1[i] ^ ar2[i])
+    return res
 
-    print(bytes(keystream))
-    print (len(msg))
+
+# generator yielding next 32 Bytes of array
+def get_part(arr):
+    for i in range(0, len(arr), N_B):
+        yield arr[i:i + N_B]
+
+# applying xor step by step with a fresh keystream
+def decrypt(secret, key):
+    decrypted = []
+    for x in get_part(secret):
+        decrypted.append(xor(x, key))
+        key = step(int.from_bytes(bytes(key), 'little')).to_bytes(N_B, 'little')
+    return decrypted
+
+# initial decryption of files from extracted keystream aka attack
+def decrypt_from_keystream():
+    plain = open('bis.txt', 'rb').read(N_B)
+    secret = open('bis.txt.enc', 'rb').read(N_B)
+
+    # use known plaintext attack to get first 32B of keystream
+    kstrm = xor(plain,secret)
+
+    # use obtained keystream and step to decrypt the rest of the algorithm
+    super_cipher = open('super_cipher.py.enc', 'rb').read()
+    print(''.join([bytes(x).decode() for x in decrypt(super_cipher, kstrm)]))
+
+    # same fo the hint.gif
+    hint = open('hint.gif.enc', 'rb').read()
+    with open('hint.gif', 'wb') as f:
+        for x in decrypt(hint, kstrm):
+            f.write(bytes(x))
+
 
 if __name__ == '__main__':
-    main()
+    decrypt_from_keystream()
