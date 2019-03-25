e18a4608ec4daa8d550f8e6fc108a18eee984ef3
Martin Krajnak <mkrajnak@redhat.com>
2019-03-25 11:33:38 +0100

    Update formatting

--- solution.py
+++ solution.py
@@ -8,12 +8,11 @@ N = 8 * N_B
 
 # Next keystream, taken from decrypted file
 def s_step(x):
-  x = (x & 1) << N+1 | x << 1 | x >> N-1
-  y = 0
-  for i in range(N):
-    y |= SUB[(x >> i) & 7] << i
-  import ipdb; ipdb.set_trace()
-  return y
+    x = (x & 1) << N+1 | x << 1 | x >> N-1
+    y = 0
+    for i in range(N):
+        y |= SUB[(x >> i) & 7] << i
+    return y
 
 
 # xor or two byte arrays
@@ -21,7 +20,7 @@ def xor(ar1, ar2):
     length = len(ar1) if len(ar1) < len(ar2) else len(ar2)
     res = []
     for i in range(length):
-            res.append(ar1[i] ^ ar2[i])
+        res.append(ar1[i] ^ ar2[i])
     return res
 
 
@@ -29,6 +28,7 @@ def xor(ar1, ar2):
 def get_part(arr):
     for i in range(0, len(arr), N_B):
         yield arr[i:i + N_B]
+
 
 # applying xor step by step with a fresh keystream
 def decrypt(secret, key):
@@ -38,13 +38,14 @@ def decrypt(secret, key):
         key = step(int.from_bytes(bytes(key), 'little')).to_bytes(N_B, 'little')
     return decrypted
 
+
 # initial decryption of files from extracted keystream aka attack
 def decrypt_from_keystream():
     plain = open('bis.txt', 'rb').read(N_B)
     secret = open('bis.txt.enc', 'rb').read(N_B)
 
     # use known plaintext attack to get first 32B of keystream
-    kstrm = xor(plain,secret)
+    kstrm = xor(plain, secret)
 
     # use obtained keystream and step to decrypt the rest of the algorithm
     super_cipher = open('super_cipher.py.enc', 'rb').read()
@@ -56,8 +57,9 @@ def decrypt_from_keystream():
         for x in decrypt(hint, kstrm):
             f.write(bytes(x))
 
+
 def r_step(x):
-    x = x >> 1 &  ~(1 << N)
+    x = x >> 1 & ~(1 << N)
 
 
 if __name__ == '__main__':
